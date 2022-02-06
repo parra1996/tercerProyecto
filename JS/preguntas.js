4b11d5d7db3959a382536c34ed38acc656ba8dfc
“juanP1996” <parrajp1996@gmail.com>
2022-02-06 19:44:27 +0100

    comenzando de 0

--- JS/preguntas.js
+++ JS/preguntas.js
@@ -1,43 +1,152 @@
-const nextQuestion = () => {
 
-}
+
+// ////VARIABLES
+
+
+
+// let muestraPreguntas = document.getElementById("preguntas11");
+// let muestraRespuestas = document.getElementById("respuestas3");
+
+// let botonCambio = document.getElementsByClassName("cambioPregunta");
+// /// FUNCIONES
+
+// const ponerPregunta1 = () => {
+//    muestraPreguntas.innerHTML = cuestionary[0].question1;
+// };
+
+// const ponerRespuestas1 = () => {
+    //     muestraRespuestas.innerHTML = cuestionary[0].answer1 ;
+    // };
+
+// const ponerPregunta2 = () => {
+    //     muestraPreguntas.innerHTML = cuestionary[1].question2;
+//  };
+
+//  const ponerRespuestas2 = () => {
+//      muestraRespuestas.innerHTML = cuestionary[1].answer2 ;
+//  };
+ 
+
+
+// // const verificar = parametro => {
+    
+    // //     let ra = document.getElementsByClassName('a');
+    // //     let rb = document.getElementsByClassName('b');
+    // //     let rc = document.getElementsByClassName('c');
+    // //     let rd = document.getElementsByClassName('d');
+    
+    // //     if (cuestionary.rAnswer1 == "b:4"){
+// //         console.log("aqui toy")
+// //         console.log("correcto");
+// //     } else {
+    // //         console.log("ta mal") ;
+    // //     } ;
+    
+    // // }
+    
+    // // const bien = () => {
+// //     console.log("bien")
+// // }
+
+// // const mal = () => { 
+    // //     console.log("mal")
+    // // }
+    
+    // const cambiaPregunta = () => {
+        //     ponerPregunta2();
+//     ponerRespuestas2();
+// }
+
+
+// // let resp = document.getElementById("respuestas").innerHTML = cuestionary[0].answer1 ;
+// // console.log(resp)
+
+
+// for (let i = 0 ; i< cuestionary.length ; i++){
+    //     ponerPregunta1();
+//     ponerRespuestas1();
+//     i++ ;
+
+
+// }
+
+// cambiaPregunta()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
 const cuestionary = [
     {
-        question1: "cuanto es 2+2 ? " ,
-        answer1: [2,4,7,1],
-        rAnswer1: 4
+        question:"cuanto es 2+2?",
+        answers: [4,5,9,2],
+       
     },
-////////////////////
+
     {
-        question2: "cuanto es 2+3 ? " ,
-        answer2: [2,4,7,5],
-        rAnswer2: 5
+        question: "cuanto es 2+3 ? " ,
+        answers: [2 ,4 ,7 ,5],
+        
     },
-///////////////////////
+
     {
-        question3:"cuanto es 2+4 ? " ,
-        answer3:[2,4,6,1],
-        rAnswer3: 6
+        question:"cuanto es 2+4 ? " ,
+        answer:[2 ,4 ,6 ,1]
+        
+    },
+
+    {
+        question:"cuanto es 2+6 ? " ,
+        answer:[8 ,4 ,6 ,1]
+        
+    },
+
+    {
+        question:"cuanto es 2+4 ? " ,
+        answer:[6 ,4 ,2 ,1]
+        
     }
 
-];
-
-let muestraPreguntas = document.getElementById("preguntas1");
-let muestraRespuestas = document.getElementById("respuestas1");
-
-const ponerPregunta1 = () => {
-   muestraPreguntas.innerHTML = cuestionary[0].question1;
-};
-
-const ponerRespuestas1 = () => {
-    muestraRespuestas.innerHTML = cuestionary[0].answer1 ;
-}
+]; 
 
 
-/*
+
+
     let rightAnswer;
     let currentQuestionIndex = 0; 
     let rightAnswers = 0;
@@ -46,7 +155,7 @@ const ponerRespuestas1 = () => {
 const printHTMLpregunta = (i) => {
     currentQuestionIndex++;
     let q = cuestionary[i] ;
-    let a = q.rAnswer1 ;
+    let a = q.answers ;
     rightAnswer = a ;
 
     let htmlAnswersArray = a.map(currentA =>`<p class="answer"><button onClick="evaluateAnswer('${currentA}', this)">R</button><span>${currentA}</span></p>`);
@@ -91,7 +200,7 @@ const evaluateAnswer = (answer, obj) => {
 }
 
 printHTMLpregunta(currentQuestionIndex);
-*/
+
 
 
 
